cb6598ad4ce4a6e457e6771ab236303455a13f9d
Hedgehog1991 <kriive@icloud.com>
2024-05-28 14:34:21 +0200

    map background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, {useEffect, useMemo, useRef} from "react";
+import "./App.css";
+import { OSM } from "ol/source";
+import TileLayer from "ol/layer/Tile";
+import { View } from "ol";
+import { Map } from "ol";
+import {useGeographic} from "ol/proj";
 
-import "./App.css"
-export function Application(){
+useGeographic()
+export function Application() {
+    const view = useMemo(() => new View({
+        center: [10.5, 59.7], zoom: 11
+    }), []);
+    const map = useMemo(() => new Map({
+        layers: [
+            new TileLayer({source: new OSM()}),
+        ],
+        view: view
+    }), [view]);
 
+    const mapRef = useRef(null);
 
+    useEffect(() => {
+        map.setTarget(mapRef.current)
+    }, []);
 
-    return(
-        <div>
-            Hello!!
-        </div>
+    return (
+        <>
+            <div className={"map"} ref={mapRef}></div>
+        </>
     )
 }
+
+
--- src/views/MapView.jsx
+++ src/views/MapView.jsx
@@ -1,10 +1,6 @@
-
-const MapView = () => {
-    return (
-        <section>
-            <h4>Map</h4>
-        </section>
-    )
-}
-
-export default MapView;
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+import React, {Dispatch, SetStateAction, useEffect, useMemo, useRef} from "react";
+import {Layer} from "ol/layer";
