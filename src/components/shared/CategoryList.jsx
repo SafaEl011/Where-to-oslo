6feae5b32f4fda6eb0a8de9ef99e703666b39da2
Adiiil94 <112103403+Adiiil94@users.noreply.github.com>
2024-05-29 15:00:14 +0200

    category aa

--- src/components/shared/CategoryList.jsx
+++ src/components/shared/CategoryList.jsx
@@ -14,7 +14,6 @@ const CategoryList = () => {
 
     return (
         <div className ="container mt-5">
-            <h2> Categories </h2>
             <div className="d-flex flex-wrap">
                 {categories.map((category, index) => (
                     <button key={index} className="btn btn-primary m-2">
