d7ab8a1b23ab6f8f7d631ae133be71001620c2dc
SafaEl011 <115483388+SafaEl011@users.noreply.github.com>
2024-05-30 09:29:03 +0200

    searchIcon expansion

---
+++ src/components/IconButtons/IconButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../css/IconStyles.css"
+
+const IconButton = ({onClick, children, className}) => {
+    return (
+        <button
+            className={`icon-button ${className}`}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default IconButton;
