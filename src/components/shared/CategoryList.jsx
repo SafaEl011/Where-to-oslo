dc2c60f5dfc47eb110f76a140729f8535b86838d
Adiiil94 <112103403+Adiiil94@users.noreply.github.com>
2024-05-31 11:41:20 +0200

    category aa

--- src/components/shared/CategoryList.jsx
+++ src/components/shared/CategoryList.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "/public/images/icons/kafePin_4.svg"
+
 
 const CategoryList = () => {
-  const categories = [
-    "Restaurants",
-    "Shopping",
-    "Cafes",
-    "Activities",
-    "Bars",
-    "Favorites",
-  ];
 
-  return (
-    <div className="container mt-5">
-      <div className="d-flex flex-wrap">
-        {categories.map((category, index) => (
-          <button key={index} className="btn btn-primary m-2">
-            {category}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+    const categories = [
+        "Restaurants",
+        "Shopping",
+        "Cafes",
+        "Activities",
+        "Bars",
+        "Favorites"
+    ];
+
+
+    return (
+        <div className ="categoryContainer mt-5">
+            <div className="d-flex flex-wrap justify-content-around">
+                {categories.map((category, index) => (
+                    <button key={index} className="btn btn-success m-2 col-4 ">
+                        {category}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default CategoryList;
--- src/components/shared/Top5List.jsx
+++ src/components/shared/Top5List.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Top5List = () => {
-  const top5Places = [
-    "Schous Kjeller",
-    "Frognerseteren",
-    "Asylet",
-    "Smia Galleri",
-    "Favorites",
-  ];
 
-  return (
-    <div className="container mt-5">
-      <div className="d-flex flex-wrap">
-        {top5Places.map((Top5List, index) => (
-          <button key={index} className="btn btn-primary m-2">
-            {Top5List}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+    const top5Places = [
+        "Schous Kjeller",
+        "Frognerseteren",
+        "Asylet",
+        "Smia Galleri",
+        "Favorites"
+    ];
+
+    return (
+        <div className ="container mt-5">
+            <div className="d-flex flex-wrap justify-content-around">
+                {top5Places.map((Top5List, index) => (
+                    <button key={index} className="btn btn-success m-2 pl-4 col-4">
+                        {Top5List}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Top5List;
