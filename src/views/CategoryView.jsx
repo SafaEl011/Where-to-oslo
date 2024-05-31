a0bbaa1582fddf879cbc94e9889a412d8068180f
Adiiil94 <112103403+Adiiil94@users.noreply.github.com>
2024-05-31 13:03:07 +0200

    category aa

--- src/views/CategoryView.jsx
+++ src/views/CategoryView.jsx
@@ -3,23 +3,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import CategoryList from "../components/shared/CategoryList";
+import "../css/BottomNavbar.css"
 
 const CategoryView = ({ show, handleClose }) => {
-  return (
-    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-      <Modal.Header closeButton>
-        <Modal.Title>Categories</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <CategoryList />
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    return (
+        <Modal
+            className="categoryModal" // Add the custom class here
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+        >
+            <Modal.Header closeButton>
+                <Modal.Title>Categories</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <CategoryList />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="success" onClick={handleClose}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default CategoryView;
