c7e47e39e89e15783be89253e1aa7e6ef809548d
Sanderchriss <sanderchriss@gmail.com>
2024-06-05 13:31:04 +0200

    OG is back!

--- src/views/index.jsx
+++
@@ -1,9 +0,0 @@
-import CategoryView from "./CategoryView";
-import IntroView from "./IntroView";
-import MapView from "./MapView";
-import Top5View from "./Top5View";
-import SearchView from "./SearchView";
-import SettingView from "./SettingView";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-export { IntroView, MapView, CategoryView, Top5View, SearchView, SettingView };
